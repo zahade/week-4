e1e8071fffa4e82217aa27bcbb072b4b0e50de4b
zahade <alzahad200@gmail.com>
2021-11-21 13:40:53 +0400

    Made neccessary changes broken_grades.py

--- broken_grades.py
+++ broken_grades.py
@@ -21,26 +21,29 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+# Add the int() function before input() function
+exam_two = int(input("Input exam grade two: "))
 
-exam_3 = str(input("Input exam grade three: "))
+# Add the str() function before the input() function
+exam_3 = int(input("Input exam grade three: "))
 
-grades = [exam_one exam_two exam_three]
+# Add commas to seperate the values within the list and rename variable 'exam_three' to 'exam_3'
+grades = [exam_one, exam_two, exam_3]
 sum = 0
-for grade in grade:
-  sum = sum + grade
+for grade in grades:  # Rename 'grade' variable to 'grades'
+    sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades)  # Rename 'grdes' to 'grades'
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90:  # Added a ":" after 90
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C"  # Replaced the single quote with a double quote
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else:  # Replaced elif statement with else statement
     letter_grade = "F"
 
 for grade in grades:
@@ -50,7 +53,7 @@ for grade in grades:
 
     print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F":  # Renamed 'letter-grade' varaible with 'letter_grade' and replaced 'is' operator with the '=='operator
+    print("Student is failing.")  # Added parenthesis
 else:
-    print "Student is passing."
+    print("Student is passing.")  # Added parenthesi
