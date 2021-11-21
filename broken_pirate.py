67b9ad795bdc7099d3e7ea802ec8bdc5c8da03f8
zahade <alzahad200@gmail.com>
2021-11-21 13:46:35 +0400

    made neccessary changes to broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,9 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+# Added a double quote after the statement ended
+greeting = input("Hello, possible pirate! What's the password?")
+# replaced the round bracket with a square bracket
+if greeting in ["Arrr!"]:
+    print("Go away, pirate.")
+# replaced the elif statement with an else statement
+else:
+    # indented the line of code correctly
+    print("Greetings, hater of pirates!")
